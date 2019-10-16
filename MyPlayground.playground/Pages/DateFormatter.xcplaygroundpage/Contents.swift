8971dba422ed73a4f039fe92f799f5e2bd32dbde
Mitchell Budge <mitchellbudge@gmail.com>
2019-10-15 18:57:25 -0600

    Add Number and DateFormatter notes

--- MyPlayground.playground/Pages/DateFormatter.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/DateFormatter.xcplaygroundpage/Contents.swift
@@ -2,18 +2,18 @@ import Foundation
 
 // Date Style
 
-
-
+let dateFormatter = DateFormatter()
+//dateFormatter.dateStyle = .short
 
 // Time Style
 
-
-
+//dateFormatter.timeStyle = .short
 
 // Date Format
 
-
-
+dateFormatter.dateFormat = "MM/dd/YYYY"
+let currentTime = dateFormatter.string(from: Date())
+print(currentTime)
 
 // Date Components
 
--- MyPlayground.playground/Pages/NumberFormatter.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/NumberFormatter.xcplaygroundpage/Contents.swift
@@ -2,24 +2,40 @@ import Foundation
 
 // Decimals
 
-
-
+let numberFormatter = NumberFormatter()
+numberFormatter.numberStyle = .decimal
+numberFormatter.usesSignificantDigits = true
+numberFormatter.minimumSignificantDigits = 2 // factor in all numbers
+numberFormatter.minimumIntegerDigits = 2 // factor in whole numbers
+numberFormatter.minimumFractionDigits = 2 // factor in decimal values
+let numberString = numberFormatter.string(from: 1.0)!
 
 // Percentages
 
-
-
+let percentFormatter = NumberFormatter()
+percentFormatter.numberStyle = .percent
+percentFormatter.usesSignificantDigits = true
+percentFormatter.minimumSignificantDigits = 5
+let percentString = percentFormatter.string(from: 0.2575)!
 
 // Spellout
 
-
+let spelloutFormatter = NumberFormatter()
+spelloutFormatter.numberStyle = .spellOut
+let spelloutString = spelloutFormatter.string(from: 2)!
 
 
 // Ordinal
 
-
-
+let ordinalFormatter = NumberFormatter()
+ordinalFormatter.numberStyle = .ordinal
+let ordinalString = ordinalFormatter.string(from: 32)!
 
 // Currency
+
+let currencyFormatter = NumberFormatter()
+currencyFormatter.numberStyle = .currency
+currencyFormatter.locale = Locale(identifier: "JP_jp")
+let currencyString = currencyFormatter.string(from: 14)!
 
 
